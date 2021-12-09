0ec1907e3cf3b0845a67f0799f199da02ac02382
Judith <judithvdw@users.noreply.github.com>
2021-12-09 13:43:29 +0100

    unedited day 9

---
+++ day9/9.py
@@ -0,0 +1,56 @@
+from collections import OrderedDict, Counter
+from functools import reduce
+
+
+def low_point(cord, heightmap):
+    x, y = cord
+    all_options = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    actual_options = [option for option in all_options if option in heightmap]
+    for option in actual_options:
+        if heightmap[option] <= heightmap[cord]:
+            return False
+    return True
+
+
+def get_low_points(heightmap):
+    low_points = {}
+    for cord, n in heightmap.items():
+        if n == 0 or low_point(cord, heightmap):
+            low_points[cord] = n
+    return low_points
+
+
+def get_map(raw):
+    d = {}
+    for i, line in enumerate(raw):
+        for j, num in enumerate(line.strip()):
+            d[(i, j)] = int(num)
+    return d
+
+
+if __name__ == '__main__':
+    with open('9.txt') as f:
+        raw = f.readlines()
+        heightmap = get_map(raw)
+
+    low = get_low_points(heightmap)
+    print(f"Part 1: {sum(low.values()) + len(low)}")
+
+    ordered_heightmap = OrderedDict(sorted(heightmap.items(), key=lambda x: x[1]))
+    basin_mapping = {k: k for k, v in low.items()}
+
+    for k, v in ordered_heightmap.items():
+        if k in basin_mapping:
+            continue
+        elif v == 9:
+            break
+        else:
+            x, y = k
+            all_options = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            actual_options = {option: heightmap[option] for option in all_options if option in heightmap}
+            small = min(actual_options, key=actual_options.get)
+            basin_mapping[k] = basin_mapping[small]
+
+    c = Counter(basin_mapping.values())
+
+    print(reduce((lambda x, y: x * y), sorted(dict(c).values(), reverse=True)[:3]))
