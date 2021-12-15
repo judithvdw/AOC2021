69295e2fc84a008cce3eda810ce398ed8d80f5ee
Judith <judithvdw@users.noreply.github.com>
2021-12-15 21:11:31 +0100

    day 14 complete

--- d14.py
+++ d14.py
@@ -1,31 +1,48 @@
-from collections import Counter
+from collections import defaultdict
+
 
 def parse_input(raw):
-    polymer = raw[0].strip()
+    polymer = defaultdict(int)
+    polymer_str = raw[0].strip()
+    for i in range(len(polymer_str) - 1):
+        polymer[polymer_str[i:i + 2]] += 1
+
     rules = {}
     for line in raw[2:]:
         k, v = line.strip().split(" -> ")
         rules[k] = v
-    return polymer, rules
+    return polymer, rules, polymer_str[-1]
 
-def grow_polymer(polymer):
-    new_polymer = ""
-    for i in range(len(polymer) -1):
-        pair = polymer[i:i+2]
+
+def grow_polymer(polymer, rules):
+    new_polymer = defaultdict(int)
+    for pair, num in polymer.items():
         insert = rules[pair]
-        new_polymer += polymer[i] + insert
-    new_polymer += polymer[-1]
+        new_polymer[pair[0] + insert] += num
+        new_polymer[insert + pair[1]] += num
     return new_polymer
+
+
+def elemental_decomposition(polymer):
+    decomposed = defaultdict(int)
+    for k, v in polymer.items():
+        decomposed[k[0]] += v
+    return decomposed
+
+
+def run(polymer, rules, last, i):
+    for _ in range(i):
+        polymer = grow_polymer(polymer, rules)
+
+    decomposed = elemental_decomposition(polymer)
+    decomposed[last] += 1
+    return max(decomposed.values()) - min(decomposed.values())
 
 
 if __name__ == '__main__':
     with open("input/14.txt") as f:
         raw = f.readlines()
-        polymer, rules = parse_input(raw)
-        print(polymer, rules)
+        polymer, rules, last = parse_input(raw)
 
-    print(len(polymer))
-    for i in range(20):
-        polymer = grow_polymer(polymer)
-        c = Counter(polymer)
-        print(i, c.most_common())
+    print(f"Part 1: {run(polymer, rules, last, 10)}")
+    print(f"Part 2: {run(polymer, rules, last, 40)}")
